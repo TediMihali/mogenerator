d15062de061d8e8343e137bdbff3862a415b5791
Tedi <tedimihali1@gmail.com>
2023-06-28 22:05:53 +0200

    Adding functions for generating arithmetic and geometric progressions and a readme file.

--- mpgenerator/password_generator.py
+++ mpgenerator/password_generator.py
@@ -9,13 +9,37 @@ def generate_password(security: int = 1) -> str:
         password = "".join(random.choices(string.ascii_letters + string.digits, k=8))
     elif security == 1:
         password = "".join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=12))
+    elif security == 2:
+        password = "".join(
+            random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits + '!?*&$', k=12))
     else:
-        password = "".join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=4 * security))
+        password = "".join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits + '!?*&$',
+                                          k=4 * security))
     return str(password)
 
 
 def generate_number(digits: int) -> int:
-    number = "".join(random.choices(string.digits, k = digits))
+    number = "".join(random.choices(string.digits, k=digits))
     return int(number)
 
-print(generate_number(5))
+
+def generate_arithmetic_progression(first_term: int = 1, diff: int = 1, number_of_terms: int = 10) -> list:
+    prog = []
+    for i in range(1, number_of_terms + 1):
+        prog.append(first_term + (i - 1) * diff)
+    return prog
+
+
+def n_term_arithmetic_progression(first_term: int = 1, diff: int = 1, n: int = 1) -> int:
+    n_term =  first_term + (n - 1) * diff
+    return n_term
+
+def generate_geometric_progression(first_term: int = 1, q: int = 2, number_of_terms: int = 10) -> list:
+    prog = []
+    for i in range(number_of_terms):
+        prog.append(first_term * q ** i)
+    return prog
+
+def n_term_geometric_progression(first_term: int = 1, q: int = 2, n: inr = 1) -> list:
+    n_term = first_term * q ** (n - 1)
+    return n_term
---
+++ readme.md
@@ -0,0 +1,20 @@
+This is a library for genearting multiple types of progressions, 
+numbers and passwords. The code is open-source and you can 
+give me ideas or contribute directly through github.
+
+Generating_arithmetic_progression:
+- generate_arithmetic_progression(first_term, step, number_of_terms) -> returns list
+
+Finding the n-th term of an arithmetic progression:
+- n_term_arithmetic_progression(first_term, step, index_of_term) -> returns int
+
+Generating_geometric_progression:
+- generate_geometri_progression(first_term, coefficient, number_of_terms) -> returns list
+
+Finding the n-th term of an arithmetic progression:
+- n_term_geometric_progression(first_term, coefficient, index_of_term) -> returns int
+
+Generating a password:
+- generate_password(security: int) -> returns string:
+
+- generate_number(digits: int) -> returns int:
